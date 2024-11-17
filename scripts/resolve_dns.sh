4ddc3c55ecbe3c25635908d0e17052bdc894e841
Ovidiu Ionescu <ovidiu@localhost.localdomain>
2024-11-17 19:25:23 +0100

    Tell the system where the dns server is on dns-stub

--- scripts/resolve_dns.sh
+++ scripts/resolve_dns.sh
@@ -3,6 +3,9 @@
 # network adapter for all other domains.
 #
 NT=$(ip route get 9.9.9.9 | grep dev | cut -d ' ' -f5)
+echo "Using network adapter for general DNS: $NT"
+
+sudo resolvectl dns dns-stub 192.168.0.10
 
 sudo resolvectl domain dns-stub ${1:-simulacron.eu}
 sudo resolvectl domain $NT '~.'
